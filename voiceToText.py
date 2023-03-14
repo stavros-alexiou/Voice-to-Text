dff3d111771b4fe1246c22a3b56f038b3435644e
Stavros El. Alexiou <stavros.alexiou13@gmail.com>
2023-03-14 15:59:30 +0200

    v1.1

--- voiceToText.py
+++ voiceToText.py
@@ -1,15 +1,26 @@
-from transformers import pipeline
+import torch
+from transformers import AutoProcessor, AutoModelForCTC
+import speech_recognition as sr
+import io
 
-def voiceToText(inputSample):
-    cls = pipeline("zuu/automatic-speech-recognition")
-    res = cls(inputSample)
-    print(res)
-
+# Alternative model
 # from transformers import AutoProcessor, AutoModelForCTC
+# processor = AutoProcessor.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
+# model = AutoModelForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
 
-# processor = AutoProcessor.from_pretrained("zuu/automatic-speech-recognition")
+processor = AutoProcessor.from_pretrained("zuu/automatic-speech-recognition")
+model = AutoModelForCTC.from_pretrained("zuu/automatic-speech-recognition")
 
-# model = AutoModelForCTC.from_pretrained("zuu/automatic-speech-recognition")
+recognition = sr.Recognizer()
 
-# res = model("sample1.mp3")
-# print(res)
+audio = "sample1.mp3"
+data = audio.get_wav_data()
+clip = AudioSegment.from_file(data)
+x = torch.FloatTensor(clip.get_array_of_samples())
+
+inputs = processor(x, sampling_rate = 16000, return_tensors='pt', padding='longest').input_values
+logits = model(inputs).logits
+tokens = torch.argmax(logits, axis = -1)
+text = processor.batch_decode(tokens) 
+
+
